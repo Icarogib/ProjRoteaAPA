dc74b82ec1dd28f30ea7eabfc156ca3e42e6f90e
matheusflc <joaomatheus012@hotmail.com>
2023-10-07 18:21:47 -0300

    base guloso v2 alterando o valor da matriz pra 0 e armazenando a cidade

--- Guloso.cpp
+++ Guloso.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 int main (){	
     ReadaOut infos; 
-    int vic, i, j, caminhoAtual, atual, custoAtual, menorcusto, caminhoProx;
+    int vic, caminhoAtual, atual, custoAtual, menorcusto, caminhoProx, auxdoJ;
     vector<int> passou;
+    vector<int> veiculocaminho;
     infos.lerValor();
 
+
     atual = custoAtual = 0;
+
 
    
     //cout << "Maior Custo: " << menorcusto << endl;
@@ -16,29 +19,44 @@ int main (){
     
     //for (vic = 0; vic < infos.veiculos; vic++){
         
-    i = caminhoAtual = 0; // declara q cada veiculo comeca do 0
-    do{
+    caminhoAtual = 0; // declara q cada veiculo comeca do 0
+    
+    for(int i = 0; i<2 ; i++){
 	
         menorcusto = infos.maiorCusto;  // inicializa que o menor custo eh o maior
 
         if( i != 0 )
             caminhoAtual = caminhoProx;
 
-        for( j = 0; j < infos.entregas; j++ ){
+        for( int j = 0; j < infos.custoij.size() ; j++ ){
 
             custoAtual = infos.custoij[caminhoAtual][j];
             
             if( custoAtual && custoAtual < menorcusto ){
+                //veiculo[vic].pushback[j]
+                //custoDoCaminho veiculo 
                 menorcusto = custoAtual;
                 caminhoProx = j;
+                auxdoJ = j;
             }
         }
+        veiculocaminho.push_back(auxdoJ);  
+        atual += custoAtual;
+        infos.custoij[caminhoAtual][auxdoJ] = 0;
+        infos.custoij[auxdoJ][caminhoAtual] = 0;
         cout << "menorcusto: " << menorcusto << "\ncaminhoprox: " << caminhoProx << endl ;
-    
-    }while(i++ != 20);
 
+    }
 
-    
+    for (int i = 0; i<infos.custoij.size(); i++){
+        for(int j = 0 ; j< infos.custoij.size(); j++){
+            cout << " " << infos.custoij[i][j];
+        }
+        cout  << endl;
+    }
+    for(auto& k: veiculocaminho){
+        cout << k << " ";
+    }
     
     return 0;
 }
