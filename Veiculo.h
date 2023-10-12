e7eb226bb8a4abcc245c3f6c37368605f271d1af
Icarogib <icarogib@gmail.com>
2023-10-12 10:32:10 -0300

    Adicionado vector a veiculos com o custo de cada viagem (aresta), incluindo a volta (ultimo valor do vector)

--- Veiculo.h
+++ Veiculo.h
@@ -6,8 +6,10 @@ class Veiculo {
 
         int carga;      // Carga atual do veiculo
         int custoCaminho;   // custo do caminho
-        bool usado;         // se veiculo foi utilizado
-        
         std::vector<int> rota; //"array" do caminho percorrido
+        std::vector<int> custoRota; // vector com o custo do caminho percorrido rota a rota
+        
+        // o ultimo valor do custoRota eh a volta do ponto que ele estava para o 0
+        // vai ser um a mais comparado ao rota
 
 };
